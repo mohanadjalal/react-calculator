0e9b8bd6d4d2ac78aa0924c67da571e98a1915da
Mohannad Jayousi <jaymohanad@gmail.com>
2021-07-02 15:56:02 +0300

    load the react files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react' ; 
+ 
+ 
+ 
+
+
+function App(){
+  return (
+    <div className="container">
+       test t
+      </div>
+  );
+}
+
+export default App ; 
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import React from 'react' ; 
+import ReactDOM from 'react-dom' ; 
+
+import App from './components/App'
+
+import './style.css'
+ 
+ 
+render(<App />, document.getElementById("root")) ; 
+
